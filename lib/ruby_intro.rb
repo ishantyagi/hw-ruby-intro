70cc4c17e34eee61642e08edf552876419ad5a94
ISHAN TYAGI <ishantyagi1992@gmail.com>
2018-02-14 01:53:07 +0000

    try1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,64 @@
 
 # Part 1
 
-def sum arr
+def sum(in_array)
   # YOUR CODE HERE
+  
+   return 0 if in_array.length == 0
+  return in_array.reduce(:+)
 end
 
-def max_2_sum arr
+def max_2_sum(int_array)
   # YOUR CODE HERE
+  return 0 if int_array.length == 0
+  return int_array[0] if int_array.length == 1
+  int_array.sort!.reverse!
+  return int_array[0] + int_array[1]  
 end
 
-def sum_to_n? arr, n
+def sum_to_n?(int_array,n)
   # YOUR CODE HERE
+  return false if int_array.length == 0 
+  int_array.each_with_index{ |val, index| 
+      return true if int_array.find_index(n - val) && int_array.find_index(n - val ) != index
+    }
+  return false # default return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
+  
 end
 
-def starts_with_consonant? s
+def starts_with_consonant?(s)
   # YOUR CODE HERE
+    !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
 end
 
-def binary_multiple_of_4? s
+def binary_multiple_of_4?(s)
   # YOUR CODE HERE
+   if s =~ /^[0-1]+$/
+    return s.to_i(2) % 4 == 0 
+   end
+  false
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+attr_accessor :isbn, :price
+
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
 end
