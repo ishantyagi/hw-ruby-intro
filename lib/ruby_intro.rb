634fb49974570624f023000b7436d9318b8c44ed
ISHAN TYAGI <ishantyagi1992@gmail.com>
2018-02-14 04:15:03 +0000

    try1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -42,11 +42,12 @@ end
 
 def starts_with_consonant?(s)
   # YOUR CODE HERE
-    !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
+    !!(s[0] =~ /[zyxwvtsrpbcdfghjklmnp]+/i)
 end
 
 def binary_multiple_of_4?(s)
   # YOUR CODE HERE
+  
    if s =~ /^[0-1]+$/
     return s.to_i(2) % 4 == 0 
    end
